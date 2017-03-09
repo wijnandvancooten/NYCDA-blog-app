eb4650c35bfc6c0d67ccb4c78b12a8e48144224e
wijnandvancooten <wijnandvancooten@hotmail.com>
2017-03-09 15:38:56 +0100

    show all posts, show single post.

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ app.use(express.static('public'))
 
 // tels the app to use sessions. The session will be max 1 hour
 app.use(session({
-  secret: 'salad gelore',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false,
-  maxAge: 1000 * 60 * 60
- }
+    secret: 'salad gelore',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 60
+    }
 }))
-
 
 //create a table in de database Users
 let users = db.define('users', {
@@ -36,6 +36,28 @@ let users = db.define('users', {
     lastname: sequelize.STRING,
     email: sequelize.STRING
 })
+
+//create a table in de database posts. Posts belong to users
+let posts = db.define('posts', {
+    title: sequelize.STRING,
+    body: sequelize.TEXT
+})
+
+//create a table in de database comments. Comments belong to posts and users
+let comments = db.define('comments', {
+    body: sequelize.STRING
+})
+
+//setting the relationship of the tables
+posts.belongsTo(users)
+posts.hasMany(comments)
+
+comments.belongsTo(users)
+comments.belongsTo(posts)
+
+users.hasMany(posts)
+users.hasMany(comments)
+
 
 //tels the app to use morgan//
 app.use(morgan('dev'));
@@ -48,9 +70,27 @@ app.use(bodyParser.urlencoded({
 //sets the view engine to pug. pug renders the html page//
 app.set('view engine', 'pug')
 
+
 //renders the pugfile allposts.pug on the root directory
 app.get('/', (req, res) => {
-    res.render('allposts')
+    posts.findAll().then((posts) => {
+        //console.log(posts)
+        //render the allposts.pug file, in the views folder
+        //posts object is passed as a parameter (key and value)
+        res.render('allposts', {
+            posts: posts
+        })
+    })
+})
+
+//gets the unique id to show one post
+app.get('/onepost/:id', (req, res) => {
+  //find the full row of data in 'posts' table from the id
+  posts.findById(req.params.id).then(post => {
+    //renders the onepost.pug file
+    console.log(post)
+    res.render('onepost', { post: post })
+  })
 })
 
 //renders the register pug file on url /register//
@@ -58,39 +98,46 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
+//renders the register pug file on url /register//
+app.get('/createpost', (req, res) => {
+    res.render('createpost')
+})
+
 //renders the dasgboard pug file on url /dashboard//
 app.get('/dashboard', (req, res) => {
     if (req.session.visited == true) {
-      res.render('dashboard', {results: req.session.user})
-      console.log ("good job by " + req.session.user.username)
+        res.render('dashboard', {
+            results: req.session.user
+        })
+        console.log("good job by " + req.session.user.username)
     } else {
-      res.redirect('/')
-      console.log ("log in fucker!")
+        res.redirect('/')
+        console.log("log in fucker!")
     }
 })
 
 //login post for users. checks data in database and compares it to the input form the body
-app.post ('/', (req, res) => {
-  //console.log(req.body)
-  //console.log('username: ' + username + ' password: ' + password)
-  users.findOne({
-    where: {
-      username:req.body.username
-    }
-  }).then(user => {
-    if (user.password == req.body.password) {
-      //console.log ('loged in: ' + req.body.username)
-      //console.log('session before', req.session)
-      //sets the session visited to true after login
-      req.session.visited = true
-      //stores the users data in the session after login. Data can be uses in dashboard
-      req.session.user = user
-      res.redirect('/dashboard')
-      //console.log('session after',req.session)
-    } else {
-      res.render('wronglogin')
-    }
-  })
+app.post('/', (req, res) => {
+    //console.log(req.body)
+    //console.log('username: ' + username + ' password: ' + password)
+    users.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(user => {
+        if (user.password == req.body.password) {
+            //console.log ('loged in: ' + req.body.username)
+            console.log('session before', req.session)
+            //sets the session visited to true after login
+            req.session.visited = true
+            //stores the users data in the session after login. Data can be uses in dashboard
+            req.session.user = user
+            res.redirect('/dashboard')
+            console.log('session after', req.session)
+        } else {
+            res.render('wronglogin')
+        }
+    })
 })
 
 //sending userdata to database blogapp table users
@@ -110,11 +157,28 @@ app.post('/register', (req, res) => {
     res.redirect('/')
 })
 
+//function to make posts
+app.post('/createpost', (req, res) => {
+    if (req.session.visited == true) {
 
+        let newPost = {
+            title: req.body.title,
+            body: req.body.body,
+            userId: req.session.user.id
+        }
+        posts.create(newPost)
+        res.redirect('/')
+        console.log("your post: " + newPost)
+    } else {
+        res.redirect('/')
+        console.log("log in fucker!")
+    }
+})
 
 //server is running on port 3000//
 db.sync().then(() => {
-    app.listen(3001, function() {
-        console.log('Web server started on port 3001')
-    })
+    console.log('Database synced')
+}).catch(console.log.bind(console))
+app.listen(3001, function() {
+    console.log('Web server started on port 3001')
 })
